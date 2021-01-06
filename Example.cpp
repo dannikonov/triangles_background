2001d1a8e64be2a044ce0bafed3fbc5de27cf302
Danil Nikonov <dan.nikonov@gmail.com>
2021-01-06 13:16:47 +0300

    feat: example modify

--- Example.cpp
+++ Example.cpp
@@ -5,7 +5,9 @@
 // public
 Example::Example(std::string filename)
         : _filename(std::move(filename)) {
-    _size = cv::Size_(400, 400);
+    int r = 600;
+    int c = 600;
+    _size = cv::Size_(r, c);
     _img = cv::Mat::zeros(_size, CV_8UC3);
 
     // init colors
@@ -13,23 +15,47 @@ Example::Example(std::string filename)
     cv::Scalar green = cv::Scalar(0, 255, 0);
 
     // init area
-    cv::rectangle(_img, cv::Rect(50, 50, 300, 300), green, -1);
-    cv::circle(_img, cv::Point(200, 200), 100, red, -1);
+    cv::rectangle(_img, cv::Rect(0.1 * c, 0.1 * r, 0.8 * c, 0.8 * r), green, -1);
+    cv::circle(_img, cv::Point(0.5 * c, 0.5 * r),  r / 5, red, -1);
 
     // init mask
-    _mask = cv::Mat::zeros(_size, CV_8UC3);
+    _mask = cv::Mat(_size, CV_8U, cv::Scalar(0));
     cv::Point triangle[3] = {
-            cv::Point(_size.width, 0),
             cv::Point(_size.width, _size.height),
+            cv::Point(_size.width, 0),
             cv::Point(0, _size.height),
     };
-    cv::fillConvexPoly(_mask, triangle, 3, cv::Scalar(255, 255, 255));
+
+    add_gradient_triangle(_mask, triangle);
+}
+
+void Example::add_triangle(cv::Mat &target, cv::Point *points) {
+    cv::fillConvexPoly(target, points, 3, cv::Scalar(255));
+}
+
+void Example::gradient_section(cv::Mat &target, cv::Point *points) {
+    std::sort(points, points + 3, [](cv::Point a, cv::Point b) { return a.y < b.y; });
+
+    int ymin = points[0].y;
+    int ymax = points[2].y;
+
+    int rows = ymax - ymin + 1;
+
+    int color_start = 0;
+    int color_end = 255;
+    // @todo support gradient direction
+
+    int color;
+    for (int r = ymin, i = 0; r < ymax; r++, i++) {
+        color = static_cast<int> (round((double) ((color_end - color_start) * i) / rows));
+        target.row(r).setTo(cv::Scalar(color));
+    }
 }
 
 void Example::bw_with_mask() {
     cv::Mat bw;
-    cv::cvtColor(_img, bw, cv::COLOR_RGB2GRAY);
-    cv::cvtColor(bw, bw, cv::COLOR_GRAY2RGB);
+    cv::cvtColor(_img, bw, cv::COLOR_BGR2GRAY);
+    cv::cvtColor(bw, bw, cv::COLOR_GRAY2BGR);
     bw.convertTo(bw, CV_8UC3);
 
     cv::Mat result;
@@ -40,10 +66,13 @@ void Example::bw_with_mask() {
         cv::Mat roi_f(bw, row);
 
         cv::Mat row_mask(_mask, row);
-        cv::Mat roi_r = roi_f & row_mask;
+        cv::Mat roi_r;;
 
-        double alpha = 0.5;
-        cv::addWeighted(roi_i, alpha, roi_r, 1 - alpha, 0, roi_r);
+        cv::Scalar c = cv::mean(row_mask);
+        double alpha = c[0] / 255;
+
+        cv::addWeighted(roi_i, alpha, roi_f, 1 - alpha, 0, roi_r);
+
         result.push_back(roi_r);
     }
 
@@ -66,8 +95,16 @@ void Example::mask_use_alpha() {
 
     cv::Mat img_bgra;
     cv::cvtColor(_img, img_bgra, cv::COLOR_BGR2BGRA);
+}
 
+void Example::add_gradient_triangle(cv::Mat &layer, cv::Point *triangle) {
+    cv::Mat mask(layer.size(), layer.type());
+    add_triangle(mask, triangle);
 
+    cv::Mat gradient(layer.size(), layer.type());
+    gradient_section(gradient, triangle);
+
+    gradient.copyTo(layer, mask);
 }
 
 void Example::show() {
--- Example.h
+++ Example.h
@@ -22,9 +22,16 @@ private:
 public:
     Example(std::string filename);
 
+
+    void add_triangle(cv::Mat &, cv::Point *);
+
+    void gradient_section(cv::Mat &, cv::Point *);
+
     void bw_with_mask();
 
     void mask_use_alpha();
+
+    void add_gradient_triangle(cv::Mat &target, cv::Point *);
 
     void show();
 
